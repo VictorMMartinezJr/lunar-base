fc1a21ffdc8909d1f0fe401a3372479239a1be96
Victor <victorm.martinezjr@outlook.com>
2026-05-23 17:04:23 -0400

    chore(constants): add NAVLINKS data

---
+++ src/constants/index.tsx
@@ -0,0 +1,22 @@
+interface Link {
+  text: string;
+  href: string;
+}
+export const NAVLINKS: Link[] = [
+  {
+    text: "HOME",
+    href: "/",
+  },
+  {
+    text: "ARCHITECTURE",
+    href: "/architecture",
+  },
+  {
+    text: "PHASES",
+    href: "/phases",
+  },
+  {
+    text: "MARS-FORWARD",
+    href: "/mars",
+  },
+];

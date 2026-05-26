c068cdb7dea0c2f91fb3393f02204e088ddf90f5
Victor <victorm.martinezjr@outlook.com>
2026-05-26 19:34:38 -0400

    add sections to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import gsap, { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+import LunarBase from "./sections/LunarBase";
+import Phases from "./sections/Phases";
+import Mars from "./sections/Mars";
+import Footer from "./sections/Footer";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
 const App = () => {
-  return <div>App</div>;
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
+
+  return (
+    <main>
+      <Navbar />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <Hero />
+          <LunarBase />
+          <Phases />
+          <Mars />
+          <Footer />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default App;

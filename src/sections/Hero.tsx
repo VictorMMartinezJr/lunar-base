85d621beb5d051e97c6458602ce5b058342c965e
Victor <victorm.martinezjr@outlook.com>
2026-05-26 02:00:04 -0400

    feat(ui): add meteors to Hero sections

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/SplitText";
+import Meteors from "../components/Meteors";
 
 const Hero = () => {
   useGSAP(() => {
@@ -16,9 +17,7 @@ const Hero = () => {
       ease: "power2.inOut",
     });
 
-    tl.from(".glass-bg", { opacity: 0 })
-
-      .from(moonSplit.chars, { opacity: 0, y: 20, stagger: 0.02 })
+    tl.from(moonSplit.chars, { opacity: 0, y: 20, stagger: 0.02 })
       .from(".hero-img", { opacity: 0 })
       .from(nasaSplit.chars, {
         opacity: 0,
@@ -31,26 +30,23 @@ const Hero = () => {
   });
 
   return (
-    <section className="relative overflow-x-hidden h-dvh flex flex-col justify-center items-center text-center text-white">
-      <h1 className="relative moon-title font-bold z-20 text-2xl xs:text-3xl -translate-y-40 md:-translate-y-90">
-        The Return To The Moon
-      </h1>
+    <section className="hero relative overflow-hidden h-dvh col-center text-center text-white">
+      <Meteors />
+      <h1 className="moon-title">The Return To The Moon</h1>
 
-      <h2 className="nasa-title absolute z-20 font-bold text-8xl xs:text-9xl sm:text-[200px] md:text-[250px] xl:text-[400px] 2xl:text-[550px] text-shadow-md text-shadow-white">
-        NASA
-      </h2>
+      <h2 className="nasa-title">NASA</h2>
 
       {/* Bg orbs */}
       <div className="orbs">
-        <div className="orb-1 z-5 absolute -right-5 top-50lg:top-90 w-20 h-20 bg-linear-to-tr from-white to-gray-500 rounded-full shadow-white blur-3xl"></div>
-        <div className="orb-2 z-5 absolute -left-5 bottom-40 w-20 h-20 bg-linear-to-tr from-white to-gray-500 rounded-full shadow-white blur-3xl"></div>
-        <div className="orb-3 z-5 absolute right-50 top-20  w-20 h-20 bg-linear-to-tr from-white to-gray-500 rounded-full shadow-white blur-3xl"></div>
+        <div className="orb orb-1"></div>
+        <div className="orb orb-2"></div>
+        <div className="orb orb-3"></div>
       </div>
       {/* Video */}
       <img
         src="/assets/images/hero.png"
         alt="Astronaut holding the moon in his hands"
-        className="hero-img absolute bottom-0 object-contain z-20 scale-150 sm:scale-100"
+        className="hero-img absolute bottom-0 object-contain z-20 scale-150 sm:scale-100 md:max-w-2xl"
       />
       <video
         src="/assets/videos/hero.mp4"

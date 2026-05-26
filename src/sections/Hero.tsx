4a454710a2a71a81e388bab5b2158259a284d497
Victor <victorm.martinezjr@outlook.com>
2026-05-26 15:33:32 -0400

    refactor(Hero): remove unused IsVideoLoaded state

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,7 +5,7 @@ import Meteors from "../components/Meteors";
 import { useState } from "react";
 
 const Hero = () => {
-  const [isVideoLoaded, setIsVideoLoaded] = useState<boolean>(false);
+  const [, setIsVideoLoaded] = useState<boolean>(false);
 
   useGSAP(() => {
     const nasaSplit = SplitText.create(".nasa-title", {
@@ -39,9 +39,19 @@ const Hero = () => {
       className="hero relative overflow-hidden h-dvh col-center text-center text-white"
     >
       <Meteors />
-      <h1 className="moon-title">The Return To The Moon</h1>
+      <h1 className="moon-title font-michroma">The Return To The Moon</h1>
 
-      <h2 className="nasa-title">NASA</h2>
+      <h2
+        className="nasa-title font-inter"
+        style={{
+          filter: `
+            drop-shadow(0 0 5px rgba(255, 255, 255, 0.2))  /* Inner white glow */
+            drop-shadow(0 0 15px rgba(255, 255, 255, 0.7)) /* Outer white glow */
+            drop-shadow(0 0 30px rgba(255, 255, 255, 0.1)) /* Far outer fade */ `,
+        }}
+      >
+        NASA
+      </h2>
 
       {/* Bg orbs */}
       <div className="orbs">
@@ -61,17 +71,18 @@ const Hero = () => {
         muted
         playsInline
         preload="auto"
-        poster="assets/images/hero-poster.png"
+        poster="assets/images/stars-poster.png"
         onCanPlayThrough={() => setIsVideoLoaded(true)}
         className="hero__video--1 absolute w-full h-full inset-0 object-cover z-0"
       />
       <video
-        src="/assets/videos/hero1.mp4"
+        src="/assets/videos/moving-stars.mp4"
         autoPlay
         muted
         playsInline
+        loop
         preload="auto"
-        poster="assets/images/hero-poster.png"
+        poster="assets/images/stars-poster.png"
         className="hero__video--2 absolute w-full h-full inset-0 object-cover z-0 opacity-0"
       />
     </section>

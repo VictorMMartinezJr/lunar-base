f4789f106517652d2b8d5023abfff52e2716098c
Victor <victorm.martinezjr@outlook.com>
2026-06-07 23:34:05 -0400

    refactor(ui): remove unused Title from Phases

--- src/sections/Phases.tsx
+++ src/sections/Phases.tsx
@@ -4,9 +4,6 @@ const Phases = () => {
   return (
     <section id="phases" className="phases-section">
       <div className="h-full flex xl:flex-row flex-col items-center relative">
-        {/* <div className="lg:w-[57%] flex-none h-80 lg:h-full md:mt-20 xl:mt-0">
-          <FlavorTitle />
-        </div> */}
         <div>
           <PhasesSlider />
         </div>
